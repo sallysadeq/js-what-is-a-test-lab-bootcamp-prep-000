ccf7869e6396af0fdfd8ccc202c75c057988dfb7
Sally Sadeq <sallysadeq22@gmail.com>
2018-06-24 20:51:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // write your code below!
 var name = "suzan"
 var height = 30
-var message ='${name} is ${height} inches tall'
 
-module.export(name,height,message)
+
+module.export(name,height)
 
 
 
